565dd4b39eb43bb1fa9aef21bf4c2abad5e9fa1e
fightface <amy030731@gmail.com>
2024-08-20 11:08:23 -0500

    01.082024.04

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import yfinance as yf
 from yfinance import Ticker
+import time
+from threading import Timer
 
-#SET UP TRACKER HERE:
-def tracker():
-    price = float(input("TRACKING AGAINST:  "))
-    return price
+# Store prices for different symbols
+user_input = "0"
+tracked_data = {
+    'TSLA': {'name': 'Tesla', 'price': 750.0},
+    '^GSPC': {'name': 'S&P 500', 'price': 4500.0},
+    'EUR=X': {'name': 'USD to EUR', 'price': 0.85}
+}
 
 def fiveback(symbol):
     ticker = yf.Ticker(symbol)
@@ -20,27 +25,37 @@ def nownow(symbol):
     #print(data)
     return data
 
-def color(symbol, price):
-    if nownow(symbol) > price:
+#color code based on comparison
+def color(symbol):
+    if nownow(symbol) > tracked_data[symbol]['price']:
         return "green"
     else:
         return "red"
 
-def combine(name, symbol):
+def combine(symbol, name):
     print(name,": ")
     print("five days ago close")
     print(fiveback(symbol))
     print("latest price")
     print(nownow(symbol))
     print()
-    print(color(symbol, tracker()))
+    print(color(symbol))
     print()
     print()
 
-### CALLS
-#symbols -- Telsa = TSLA, S&P 500 = ^GSPC, USD to EURO  is EUR=X
-#
-combine("Tesla", 'TSLA')
-combine("S&P 500",'^GSPC')
-combine("USD to EUR",'EUR=X')
+def refresh():
+    for symbol in tracked_data.keys():
+        combine(symbol, tracked_data[symbol]['name'])
+    Timer(30, refresh).start()
+    print("Refreshing...")
 
+def program():
+    refresh()
+    while user_input != 'x':
+        user_input = input("Enter 'X' to exit at any time.").strip().lower()
+        if user_input == 'x':
+            print("Exiting now...")
+
+##go, be free
+
+program()
