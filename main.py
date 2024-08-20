aaa199db426e08a5101c6580ebff9d4e847d6721
fightface <amy030731@gmail.com>
2024-08-20 11:15:28 -0500

    01.082024.05

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import time
 from threading import Timer
 
 # Store prices for different symbols
-user_input = "0"
 tracked_data = {
     'TSLA': {'name': 'Tesla', 'price': 750.0},
     '^GSPC': {'name': 'S&P 500', 'price': 4500.0},
     'EUR=X': {'name': 'USD to EUR', 'price': 0.85}
 }
+
+on = True
+user_input = 'q'
+
 
 def fiveback(symbol):
     ticker = yf.Ticker(symbol)
@@ -44,18 +47,22 @@ def combine(symbol, name):
     print()
 
 def refresh():
-    for symbol in tracked_data.keys():
-        combine(symbol, tracked_data[symbol]['name'])
-    Timer(30, refresh).start()
-    print("Refreshing...")
+    if on:
+        for symbol in tracked_data.keys():
+            combine(symbol, tracked_data[symbol]['name'])
+        Timer(30, refresh).start()
 
 def program():
     refresh()
+    global user_input
     while user_input != 'x':
         user_input = input("Enter 'X' to exit at any time.").strip().lower()
         if user_input == 'x':
             print("Exiting now...")
+            global on
+            on = False
+    print("Refreshing...")
+
 
 ##go, be free
-
 program()
