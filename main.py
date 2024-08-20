0b89b300e4362ed78750ebf9ccef3753178ee877
fightface <amy030731@gmail.com>
2024-08-20 09:47:45 -0500

    01.082024.02

--- main.py
+++ main.py
@@ -1,38 +1,42 @@
 import yfinance as yf
 from yfinance import Ticker
 
+#SET UP TRACKER HERE:
 
-#symbols -- Telsa = TSLA, S&P 500 = ^GSPC, USD to EURO  is EUR = X
-def weekAgo(symbol):
+
+def fiveback(symbol):
     ticker = yf.Ticker(symbol)
-    data = ticker.history(period='7d', interval='1d')
-    print(data)
+    data = ticker.history(period='5d', interval='1d')
+    #print(data)
+    close = data['Close'].iloc[0]
+    #print(close)
+    return close
 
+def nownow(symbol):
+    ticker = yf.Ticker(symbol)
+    data = ticker.fast_info.last_price
+    #print(data)
+    return data
 
-# create ticker for Tesla Stock
-tTicker = yf.Ticker('TSLA')
-# get data of the most recent date
-tData = tTicker.history(period='7d', interval='1d')
-#print data
-print("TESLA")
-print(tData)
-# Extracting the closing price
-tClose = tData['Close'].iloc[0]
-print("TESLA Closing Price:", tClose)
+def color(symbol):
+    if nownow(symbol) > fiveback(symbol):
+        return "green"
+    else:
+        return "red"
 
+def combine(name, symbol):
+    print(name,": ")
+    print(fiveback(symbol))
+    print(nownow(symbol))
+    print()
+    print(color(symbol))
+    print()
+    print()
 
+### CALLS
+#symbols -- Telsa = TSLA, S&P 500 = ^GSPC, USD to EURO  is EUR=X
+#
+combine("Tesla", 'TSLA')
+combine("S&P 500",'^GSPC')
+combine("USD to EUR",'EUR=X')
 
-spTicker = yf.Ticker('^GSPC')
-spData = spTicker.history(period='1d')
-print("S&P 500")
-print(spData)
-spClose = spData['Close'].iloc[0]
-print("S&P 500 Closing Price:", spClose)
-
-
-ueTicker = yf.Ticker('EUR=X')
-ueData = ueTicker.history(period='1d')
-print("USD to EUR")
-print(ueData)
-ueClose = ueData['Close'].iloc[0]
-print("USD to EUR Closing Price:", ueClose)
